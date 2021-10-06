7ffb9ac6f0709d977fe237db2ca81ede4da39026
Pierre BERTIN <pierre@glihmware.net>
2021-10-05 22:16:18 -0500

    add dtomapper and metacrud for basic generic operations

---
+++ DtoMapper.cs
@@ -0,0 +1,80 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Reflection;
+
+namespace NpgsqlCrud
+{
+    /// <summary>
+    ///   Very basic mapper for dtos.
+    ///   
+    ///   WARNING: due to the use of reflexivity, some error
+    ///   are only cought at RUNTIME.
+    ///   Please consider implementing FromEntity(E e) and FromEntities(List<E> es)
+    ///   for compile time checkings.
+    /// </summary>
+    public static class DtoMapper
+    {
+
+
+        /// <summary>
+        ///   Maps entity fields into the given dto.
+        /// </summary>
+        /// <typeparam name="E"></typeparam>
+        /// <typeparam name="D"></typeparam>
+        /// <param name="e"></param>
+        /// <returns> Null if entity was null, the dto otherwise. </returns>
+        public static D EntityToDto<E, D>(E e)
+            where E : class
+            where D : class, new()
+        {
+            if (e == null)
+            {
+                return null;
+            }
+
+            D dto = new();
+
+            foreach (PropertyInfo prop in dto.GetType().GetProperties())
+            {
+                PropertyInfo pe = e.GetType().GetProperty(prop.Name);
+                if (pe == null)
+                {
+                    throw new Exception($"Entity {e.GetType()} is expected to have a property {prop.Name}. (Asked by dto {dto.GetType()}).");
+                }
+
+                prop.SetValue(dto, pe.GetValue(e));
+            }
+
+            return dto;
+        }
+
+
+        /// <summary>
+        ///   Applies EntityToDto on an enumerable.
+        /// </summary>
+        /// <typeparam name="E"></typeparam>
+        /// <typeparam name="D"></typeparam>
+        /// <param name="es"></param>
+        /// <returns> Null if the IEnumerable was null, the list of dto otherwise. </returns>
+        public static List<D> EntitiesToDto<E, D>(IEnumerable<E> es)
+            where E : class
+            where D : class, new()
+        {
+            if (es == null)
+            {
+                return null;
+            }
+
+            List<D> dtos = new();
+
+            foreach (E e in es)
+            {
+                dtos.Add(EntityToDto<E, D>(e));
+            }
+
+            return dtos;
+        }
+    }
+}
---
+++ MetaCrud.cs
@@ -0,0 +1,94 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+using Npgsql;
+
+namespace NpgsqlCrud
+{
+    /// <summary>
+    ///   Some handy stuff generalized for queries.
+    /// </summary>
+    public static class MetaCrud
+    {
+
+        /// <summary>
+        ///   Retrieves an entity from a unique field comparison.
+        ///   (guid, label for instance)
+        ///   
+        ///   FIXME: deport into NpgsqlCrud...!
+        /// </summary>
+        /// <param name="what"></param>
+        /// <param name="value"></param>
+        /// <param name="conn"></param>
+        /// <returns></returns>
+        public static async Task<E>
+        FromField<E>(string what, string value, NpgsqlConnection conn = null)
+            where E : class, new()
+        {
+            WhereBuilder wb = new();
+            wb.AddClause((what, value));
+
+            (NpgsqlCrudErrno err, E e) = await Crud.ReadFirst<E>(wb: wb, conn: conn);
+            if (err != NpgsqlCrudErrno.Ok) return null;
+
+            return e;
+        }
+
+
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <typeparam name="E"></typeparam>
+        /// <param name="id"></param>
+        /// <param name="conn"></param>
+        /// <returns></returns>
+        public static async Task<E>
+        FromId<E>(int id, NpgsqlConnection conn = null)
+            where E : class, new()
+        {
+            (NpgsqlCrudErrno err, E e) = await Crud.ReadFirst<E>(id, conn: conn);
+            if (err != NpgsqlCrudErrno.Ok) return null;
+
+            return e;
+        }
+
+
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <typeparam name="E"></typeparam>
+        /// <param name="id"></param>
+        /// <param name="conn"></param>
+        /// <returns></returns>
+        public static async Task<E>
+        FromId<E>(long id, NpgsqlConnection conn = null)
+            where E : class, new()
+        {
+            (NpgsqlCrudErrno err, E e) = await Crud.ReadFirst<E>(id, conn: conn);
+            if (err != NpgsqlCrudErrno.Ok) return null;
+
+            return e;
+        }
+
+
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <typeparam name="E"></typeparam>
+        /// <param name="conn"></param>
+        /// <returns></returns>
+        public static async Task<List<E>>
+        ListAll<E>(NpgsqlConnection conn = null)
+            where E : class, new()
+        {
+            (NpgsqlCrudErrno err, List<E> e) = await Crud.Read<E>(conn: conn);
+            if (err != NpgsqlCrudErrno.Ok) return null;
+
+            return e;
+        }
+
+
+    }
+}
